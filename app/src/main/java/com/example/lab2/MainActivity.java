b4eacb94ff0695bb17400ac5410404bf89b0f0bf
Sommer Olson <olsozom@gmail.com>
2020-02-06 21:44:06 -0600

    Changes for milestone 1, app that inputs text

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        EditText myTextField = (EditText) findViewById(R.id.editText);
+
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
